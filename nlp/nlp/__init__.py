4ec499350ed7d1e82fcd436edc0ba414b8ba6402
JoshSilverb <joshsilverberg8@gmail.com>
2022-11-28 13:50:16 -0500

    Roll back to older more reliable qagen

--- nlp/nlp/__init__.py
+++ nlp/nlp/__init__.py
@@ -1,13 +1,20 @@
 import flask
 import spacy
-from transformers import pipeline, AutoModelWithLMHead, AutoTokenizer
-from nlp.qg import pipelines
+from transformers import pipeline, AutoModelWithLMHead, AutoTokenizer, AutoModelForSeq2SeqLM
+# from nlp.qg import pipelines
 
 print("Initializing server...")
 summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
 print("Summarizer loaded")
-qagen_model = pipelines.pipeline("question-generation", model="valhalla/t5-base-qg-hl")
-print("qagen model loded")
+#qagen_model = pipelines.pipeline("question-generation", model="valhalla/t5-base-qg-hl")
+# #print("qagen model loded")
+spacy_model = spacy.load("en_core_web_sm") # can be changed to "en_core_web_md/lg/trf" for better + slower
+print("Spacy model loaded")
+qg_tokenizer = AutoTokenizer.from_pretrained("mrm8488/t5-base-finetuned-question-generation-ap")
+print("Tokenizer loaded")
+qg_model = AutoModelForSeq2SeqLM.from_pretrained("mrm8488/t5-base-finetuned-question-generation-ap")
+print("QG model loaded")
+
 print("NLP server initialized")
 # app is a single object used by all the code modules in this package
 app = flask.Flask(__name__)
